9362f5b8e98a04f4c1c44f7d0e704538d450de30
cmwedin <cmwedin@gmail.com>
2022-08-08 12:46:24 -0400

    Basics
    fundamental logic needed for the commandpattern, Command class with execute method and CommandStream with a queue of commands to execute and a stack of already executed ones

---
+++ Runtime/Commands/Command.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SadSapphicGames.CommandPattern
+{
+    public abstract class Command {
+        public abstract void Execute();
+    }
+}
