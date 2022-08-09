00b1ab0e87922423123cac043a58a5945876cb3a
cmwedin <cmwedin@gmail.com>
2022-08-09 12:38:48 -0400

    NullCompositeCommand now IUndoable

--- Runtime/Commands/NullCompositeCommand.cs
+++ Runtime/Commands/NullCompositeCommand.cs
@@ -1,13 +1,25 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SadSapphicGames.CommandPattern {
-    public class NullCompositeCommand : CompositeCommand {
+    public class NullCompositeCommand : CompositeCommand, IUndoable {
         public NullCompositeCommand(int size) : base(){
             List<Command> subCommands = new List<Command>();
             for (int i = 0; i < size; i++) {
                 subCommands.Add(new NullCommand());
             }
             this.subCommands = subCommands;
+        }
+
+        public Command GetUndoCommand() {
+            var undoableCommands = 
+                from com in subCommands 
+                where com is IUndoable //? should be every command
+                select (IUndoable)com;
+            var undoCommands =
+                from com in undoableCommands
+                select com.GetUndoCommand();
+            return new CompositeCommand(undoCommands);
         }
     }
 }
