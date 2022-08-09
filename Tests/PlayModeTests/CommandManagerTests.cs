b987908d67b7ca4f11d0f30b5e7c158777ca6386
cmwedin <cmwedin@gmail.com>
2022-08-09 14:03:23 -0400

    CommandManager tests

---
+++ Tests/PlayModeTests/CommandManagerTests.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using SadSapphicGames.CommandPattern;
+
+public class CommandManagerTests
+{
+    private GameObject managerObject;
+    private SingletonCommandManager CommandManagerInstance { get => SingletonCommandManager.Instance; }
+
+    [SetUp]
+    public void SetUp() {
+        managerObject = GameObject.Instantiate(new GameObject());
+        managerObject.AddComponent<SingletonCommandManager>();
+    }
+    // A Test behaves as an ordinary method
+    [Test]
+    public void CommandManagerInstanceTest() {
+        Assert.IsNotNull(CommandManagerInstance);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator CommandManagerExecutesCommandsTest()
+    {
+        CommandManagerInstance.QueueCommand(new NullCommand());
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        Assert.AreEqual(
+            expected: 1,
+            actual: CommandManagerInstance.HistoryCount
+        );
+    }
+}
