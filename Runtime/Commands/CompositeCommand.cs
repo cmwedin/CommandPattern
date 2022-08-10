78c125897731d2d8ca16992ed780fd328c4e4135
cmwedin <cmwedin@gmail.com>
2022-08-10 11:37:42 -0400

    CompositeCommand IFailable implementation
    as a CompositeCommand can be created from any collection of Commands and any Command may be IFailable, all CompositeCommands are IFailable

--- Runtime/Commands/CompositeCommand.cs
+++ Runtime/Commands/CompositeCommand.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace SadSapphicGames.CommandPattern {
     /// <summary>
-    /// A Command that is composed of multiple child commands, all of which are executed together and leave one record in the CommandStream's history. 
+    /// A Command that is composed of multiple child commands, all of which are executed together and leave one record in the CommandStream's history.
+    /// As a CompositeCommand can be created from any collection of Commands and some of these commands may implement IFailable, all composite commands also implement IFailable.
+    /// If any sub-command of a CompositeCommand is IFailable and returns true for WouldFail(), the composite will also return true.  
     /// <remark> For more information on this type of object seek external documentation on the composite design pattern </remark>
     /// </summary>
-    public class CompositeCommand : Command {
+    public class CompositeCommand : Command, IFailable {
         protected List<Command> subCommands = new List<Command>();
 
         /// <summary>
@@ -48,6 +51,17 @@ namespace SadSapphicGames.CommandPattern {
             for (int i = 0; i < ChildCount; i++) {
                 subCommands[i].Execute();
             }
+        }
+        /// <summary>
+        /// If the CompositeCommand contains IFailable subCommands that would fail to execute
+        /// </summary>
+        /// <returns> True if any subCommand is IFailable and would fail. False if no subCommand's are IFailable or all IFailable subCommands would succeed </returns>
+        public bool WouldFail() {
+            var results =
+                from com in subCommands
+                where com is IFailable
+                select ((IFailable)com).WouldFail();
+            return results.Contains(true);
         }
     }
 }
