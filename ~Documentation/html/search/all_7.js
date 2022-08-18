78ebd6f2330ff830e3e81ce01d4c94920564f1ae
cmwedin <cmwedin@gmail.com>
2022-08-18 14:39:01 -0400

    Revert "Doxygen documentation"
    This reverts commit 69fbf2bcc4cd001a7272806be009eb5b4ca82dac.

--- ~Documentation/html/search/all_7.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['ifailable_0',['IFailable',['../interface_sad_sapphic_games_1_1_command_pattern_1_1_i_failable.html',1,'SadSapphicGames::CommandPattern']]],
-  ['instance_1',['Instance',['../class_sad_sapphic_games_1_1_command_pattern_1_1_singleton_command_manager.html#a7b601b772876033e67f1a96c1999780b',1,'SadSapphicGames::CommandPattern::SingletonCommandManager']]],
-  ['internalstream_2',['internalStream',['../class_sad_sapphic_games_1_1_command_pattern_1_1_composite_command.html#a5fa74a05f724854b58581dc660971c57',1,'SadSapphicGames::CommandPattern::CompositeCommand']]],
-  ['iundoable_3',['IUndoable',['../interface_sad_sapphic_games_1_1_command_pattern_1_1_i_undoable.html',1,'SadSapphicGames::CommandPattern']]]
-];
--- ~Documentation/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['commandstream_0',['CommandStream',['../class_sad_sapphic_games_1_1_command_pattern_1_1_command_stream.html#ad9cda071a9e6c62206aaebf5b196d301',1,'SadSapphicGames::CommandPattern::CommandStream']]]
-];
